e01fa90595175028cae35d8eca9bdf2d795874b0
dhood <dhood@users.noreply.github.com>
2017-05-15 16:49:24 -0700

    Make IPs script variables

--- user-docker/run_user_container.bash
+++ user-docker/run_user_container.bash
@@ -5,9 +5,15 @@ IMAGE_NAME="user-ariac"
 DOCKER_EXTRA_ARGS=""
 COMMAND=$1
 
+NETWORK="ariac-network"
+IP="172.18.0.20"
+SERVER_IP="172.18.0.22"
+
 docker run --rm --name ${CONTAINER} \
-  --env-file ./env.list \
-  --net ariac-network \
-  --ip 172.18.0.20 \
+  -e GAZEBO_MASTER_URI=http://${SERVER_IP}:11346 \
+  -e ROS_IP=${IP} \
+  -e ROS_MASTER_URI=http://${SERVER_IP}:11311 \
+  --ip ${IP} \
+  --net ${NETWORK} \
   ${IMAGE_NAME} \
   ${COMMAND}
