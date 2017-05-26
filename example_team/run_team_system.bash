64b10ed22d8048c2b6b950906b3bbfd34c2721ee
dhood <dhood@users.noreply.github.com>
2017-05-25 19:59:13 -0700

    Add wrapper script for delaying competitor system bringup

--- example_team/run_team_system.bash
+++ example_team/run_team_system.bash
@@ -3,13 +3,5 @@
 . ~/helloworld_ws/devel/setup.bash
 
 # Run the example node
-# TODO: do not start team's system until we know that we can communicate with the server.
-# This could be done by having a wrapper script that only calls into this script when appropriate.
-echo "Waiting for ROS master"
-until rostopic list ; do sleep 1; done
-
-sleep 10
-
-# Run the example node
 echo "Running ARIAC example node"
 rosrun ariac_example ariac_example_node
