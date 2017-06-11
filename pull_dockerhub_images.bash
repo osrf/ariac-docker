55c28674026006e0d01c6d83c99c042e17022493
dhood <dhood@users.noreply.github.com>
2017-06-10 19:01:30 -0700

    Scripts for working with dockerhub

---
+++ pull_dockerhub_images.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+ROS_DISTRO=${1-indigo}
+
+case ${ROS_DISTRO} in
+  indigo)
+    echo Using ROS distro "indigo" on ubuntu distro "trusty"
+    ;;
+  kinetic)
+    echo Using ROS distro "kinetic" on ubuntu distro "xenial"
+    ;;
+  *)
+    echo "ROS distribution unsupported: ${ROS_DISTRO}"
+    exit 1
+esac
+echo "Pulling the ARIAC competition images from dockerhub"
+
+docker pull ariac/ariac-server-${ROS_DISTRO}
+docker pull ariac/ariac-comptetitor-clean-${ROS_DISTRO}
---
+++ push_dockerhub_images.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+LIST_OF_ROS_DISTROS="indigo kinetic"
+
+for ROS_DISTRO in ${LIST_OF_ROS_DISTROS}; do
+  IMAGE_NAME=ariac-server-${ROS_DISTRO}
+  docker tag ${IMAGE_NAME} ariac/${IMAGE_NAME}:latest
+  docker push ariac/${IMAGE_NAME}:latest
+  IMAGE_NAME=ariac-competitor-clean-${ROS_DISTRO}
+  docker tag ${IMAGE_NAME} ariac/${IMAGE_NAME}:latest
+  docker push ariac/${IMAGE_NAME}:latest
+done
