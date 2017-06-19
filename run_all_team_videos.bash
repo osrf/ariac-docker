460a421a2b67e5a3c1d4d2dfbe55783eb1ae07b3
Carlos Aguero <caguero@osrfoundation.org>
2017-06-19 16:52:48 -0700

    Tweak

--- run_all_team_videos.bash
+++ run_all_team_videos.bash
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 COMP_CONFIGS_DIR=${DIR}/comp_configs/
 
-LIST_OF_TEAMS="rros_team"
+LIST_OF_TEAMS="example_team example_team2"
 
 for TEAM_NAME in ${LIST_OF_TEAMS}; do
   ./run_all_trial_videos.bash "${TEAM_NAME}"
