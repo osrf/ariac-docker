c2bc72998842c31b8cba49a51f18ad4ece06984c
dhood <dhood@users.noreply.github.com>
2018-02-13 09:06:45 -0800

    Update dockerhub images
    ariac2 in the name, tag the ariac/gazebo version in addition to latest

--- push_dockerhub_images.bash
+++ push_dockerhub_images.bash
@@ -1,15 +1,24 @@
 #!/bin/bash
-set -e
+set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-LIST_OF_ROS_DISTROS="indigo kinetic"
+LIST_OF_ROS_DISTROS="kinetic"
 
 for ROS_DISTRO in ${LIST_OF_ROS_DISTROS}; do
   IMAGE_NAME=ariac-server-${ROS_DISTRO}
-  docker tag ${IMAGE_NAME} ariac/${IMAGE_NAME}:latest
-  docker push ariac/${IMAGE_NAME}:latest
+  TAG_NAME=ariac2-server-${ROS_DISTRO}:ariac2_0.0-gazebo_7.7
+  docker tag ${IMAGE_NAME} ariac/${TAG_NAME}
+  docker push ariac/${TAG_NAME}
+  TAG_NAME=ariac2-server-${ROS_DISTRO}:latest
+  docker tag ${IMAGE_NAME} ariac/${TAG_NAME}
+  docker push ariac/${TAG_NAME}
+
   IMAGE_NAME=ariac-competitor-base-${ROS_DISTRO}
-  docker tag ${IMAGE_NAME} ariac/${IMAGE_NAME}:latest
-  docker push ariac/${IMAGE_NAME}:latest
+  TAG_NAME=ariac2-competitor-base-${ROS_DISTRO}:ariac2_0.0
+  docker tag ${IMAGE_NAME} ariac/${TAG_NAME}
+  docker push ariac/${TAG_NAME}
+  TAG_NAME=ariac2-competitor-base-${ROS_DISTRO}:latest
+  docker tag ${IMAGE_NAME} ariac/${TAG_NAME}
+  docker push ariac/${TAG_NAME}
 done
