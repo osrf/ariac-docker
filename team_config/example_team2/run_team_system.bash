9497ae84bff6f1a370ccddeb0fdd16f90ec42b5f
dhood <dhood@users.noreply.github.com>
2018-02-13 09:19:56 -0800

    Remove example_team2 (only kinetic supported)

--- team_config/example_team2/run_team_system.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-. ~/my_team_ws/install/setup.bash
-
-# Run the example node
-echo "Launching ARIAC example nodes for example_team2"
-roslaunch ariac_team_example example_nodes.launch
