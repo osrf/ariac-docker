1fa959afdc5f02d5cdfee774ce77c18b3097e328
Shane Loretz <sloretz@osrfoundation.org>
2019-05-28 15:08:23 -0700

    Flatten ariac-server into one Dockerfile
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- ariac-server/build-images.sh
+++ ariac-server/build-images.sh
@@ -8,34 +8,12 @@ set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker_images="gzserver gazebo gazebo-ros nvidia-gazebo-ros ariac-server"
+echo " Build image: ariac-server"
 
-for docker_img in ${docker_images}; do
-  echo " Build image: ${docker_img} "
+DOCKER_ARGS="--no-cache"
+USERID=`id -u $USER`
+if [[ ${USERID} != 0 ]]; then
+  DOCKER_ARGS="--build-arg USERID=${USERID}"
+fi
 
-  case ${docker_img} in
-    'ariac-server')
-      DOCKER_ARGS="--no-cache"
-      ;;
-    'gazebo')
-      USERID=`id -u $USER`
-      # If the script is run by root, do no pass 0 as the USERID to create the
-      # ariac-user. The Dockerfile defaults it to 1000
-      if [[ ${USERID} != 0 ]]; then
-        DOCKER_ARGS="--build-arg USERID=${USERID}"
-      fi
-      ;;
-    'gazebo-ros')
-      DOCKER_ARGS=""
-      ;;
-    'gzserver')
-      DOCKER_ARGS=""
-      ;;
-    *)
-      DOCKER_ARGS=""
-  esac
-
-  docker build ${DOCKER_ARGS} \
-    --tag ${docker_img}-melodic:latest \
-      $DIR/${docker_img}
-done
+docker build --force-rm ${DOCKER_ARGS} --tag ariac-server-melodic:latest $DIR/ariac-server
